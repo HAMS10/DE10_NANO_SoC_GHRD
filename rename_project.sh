f9a0f393029dce923a5878e694e73a3986c59947
hams10 <hmedinasantacruz@gmail.com>
2021-09-30 19:40:17 -0500

    Script 2 rename project

--- rename_project.sh
+++ rename_project.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-for filename in DE10_NANO_SoC_GHRD*; do sed "s/DE10_NANO_SoC_GHRD/$1/g" $filename; done
 
-for filename in DE10_NANO_SoC_GHRD*; do mv $filename ${filename//DE10_NANO_SoC_GHRD/$1}; done
+if [$1 -n]
+then
+    cd ..
 
-cd ../
-mv DE10_NANO_SoC_GHRD $1
+    cp -r DE10_NANO_SoC_GHRD $1
+
+    cd $1
+
+    for filename in DE10_NANO_SoC_GHRD*; do sed "s/DE10_NANO_SoC_GHRD/$1/g" $filename; done
+
+    for filename in DE10_NANO_SoC_GHRD*; do mv $filename ${filename//DE10_NANO_SoC_GHRD/$1}; done
+
+    rm -rf .git
+
+    rm rename_project.sh
+else
+    echo "input name invalid \"$1\""
+fi
