fadae50ba17381eff4329890151a0481f1460262
hams10 <hmedinasantacruz@gmail.com>
2021-09-30 22:39:53 -0500

    de10Nano Soc GHRD template

--- create_project.sh
+++ create_project.sh
@@ -3,6 +3,7 @@
 if [ -z "$1" ]; then
     echo "input name invalid \"$1\""
 else
+
     cd ..
     
     rm -rfd $1
@@ -11,12 +12,15 @@ else
 
     cd $1
     
+    while read -r entry;do rm -rf ${entry}; done < .gitignore
+    
     rm -rfd .git
 
     for filename in DE10_NANO_SoC_GHRD*; do sed -i "s/DE10_NANO_SoC_GHRD/$1/g" $filename $filename; done
-    for filename in DE10_NANO_SoC_GHRD*; do sed -i "s/DE10_NANO_SOC_GHRD/$1/g" $filename $filename; done
+    for filename in DE10_NANO_SOC_GHRD*; do sed -i "s/DE10_NANO_SOC_GHRD/$1/g" $filename $filename; done
     
     for filename in DE10_NANO_SoC_GHRD*; do mv $filename ${filename//DE10_NANO_SoC_GHRD/$1}; done
+    for filename in DE10_NANO_SOC_GHRD*; do mv $filename ${filename//DE10_NANO_SOC_GHRD/$1}; done
 
     rm create_project.sh
 fi
